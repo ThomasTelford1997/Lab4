646e3b24eb53831153d78a02e2a089310e55a2c2
Thomas Telford <thomastelford1997@outlook.com>
2017-11-24 13:26:15 -0500

    first commit

---
+++ Lesson_7/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Lesson_7
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.notBtn1 = new System.Windows.Forms.Button();
+			this.lbl1 = new System.Windows.Forms.Label();
+			this.notBtn2 = new System.Windows.Forms.Button();
+			this.lbl2 = new System.Windows.Forms.Label();
+			this.lbl3 = new System.Windows.Forms.Label();
+			this.notBtn3 = new System.Windows.Forms.Button();
+			this.lbl4 = new System.Windows.Forms.Label();
+			this.notBtn4 = new System.Windows.Forms.Button();
+			this.lbl5 = new System.Windows.Forms.Label();
+			this.notBtn5 = new System.Windows.Forms.Button();
+			this.txt1 = new System.Windows.Forms.TextBox();
+			this.txt2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// notBtn1
+			// 
+			this.notBtn1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.notBtn1.Location = new System.Drawing.Point(15, 24);
+			this.notBtn1.Name = "notBtn1";
+			this.notBtn1.Size = new System.Drawing.Size(228, 23);
+			this.notBtn1.TabIndex = 0;
+			this.notBtn1.Text = "I think this is a button.";
+			this.notBtn1.UseVisualStyleBackColor = true;
+			this.notBtn1.Click += new System.EventHandler(this.notBtn1_Click);
+			// 
+			// lbl1
+			// 
+			this.lbl1.AutoSize = true;
+			this.lbl1.Location = new System.Drawing.Point(15, 61);
+			this.lbl1.Name = "lbl1";
+			this.lbl1.Size = new System.Drawing.Size(0, 17);
+			this.lbl1.TabIndex = 2;
+			// 
+			// notBtn2
+			// 
+			this.notBtn2.Location = new System.Drawing.Point(15, 92);
+			this.notBtn2.Name = "notBtn2";
+			this.notBtn2.Size = new System.Drawing.Size(228, 23);
+			this.notBtn2.TabIndex = 4;
+			this.notBtn2.Text = "I also think this is a button";
+			this.notBtn2.UseVisualStyleBackColor = true;
+			this.notBtn2.Click += new System.EventHandler(this.notBtn2_Click);
+			// 
+			// lbl2
+			// 
+			this.lbl2.AutoSize = true;
+			this.lbl2.Location = new System.Drawing.Point(15, 129);
+			this.lbl2.Name = "lbl2";
+			this.lbl2.Size = new System.Drawing.Size(0, 17);
+			this.lbl2.TabIndex = 5;
+			// 
+			// lbl3
+			// 
+			this.lbl3.AutoSize = true;
+			this.lbl3.Location = new System.Drawing.Point(15, 197);
+			this.lbl3.Name = "lbl3";
+			this.lbl3.Size = new System.Drawing.Size(240, 17);
+			this.lbl3.TabIndex = 7;
+			this.lbl3.Text = "Click the button to add the numbers. ";
+			// 
+			// notBtn3
+			// 
+			this.notBtn3.Location = new System.Drawing.Point(15, 160);
+			this.notBtn3.Name = "notBtn3";
+			this.notBtn3.Size = new System.Drawing.Size(228, 23);
+			this.notBtn3.TabIndex = 6;
+			this.notBtn3.Text = "I also think this is a button";
+			this.notBtn3.UseVisualStyleBackColor = true;
+			this.notBtn3.Click += new System.EventHandler(this.notBtn3_Click);
+			// 
+			// lbl4
+			// 
+			this.lbl4.AutoSize = true;
+			this.lbl4.Location = new System.Drawing.Point(15, 265);
+			this.lbl4.Name = "lbl4";
+			this.lbl4.Size = new System.Drawing.Size(0, 17);
+			this.lbl4.TabIndex = 9;
+			// 
+			// notBtn4
+			// 
+			this.notBtn4.Location = new System.Drawing.Point(15, 228);
+			this.notBtn4.Name = "notBtn4";
+			this.notBtn4.Size = new System.Drawing.Size(228, 23);
+			this.notBtn4.TabIndex = 8;
+			this.notBtn4.Text = "I also think this is a button";
+			this.notBtn4.UseVisualStyleBackColor = true;
+			// 
+			// lbl5
+			// 
+			this.lbl5.AutoSize = true;
+			this.lbl5.Location = new System.Drawing.Point(15, 333);
+			this.lbl5.Name = "lbl5";
+			this.lbl5.Size = new System.Drawing.Size(0, 17);
+			this.lbl5.TabIndex = 11;
+			// 
+			// notBtn5
+			// 
+			this.notBtn5.Location = new System.Drawing.Point(15, 296);
+			this.notBtn5.Name = "notBtn5";
+			this.notBtn5.Size = new System.Drawing.Size(228, 23);
+			this.notBtn5.TabIndex = 10;
+			this.notBtn5.Text = "I also think this is a button";
+			this.notBtn5.UseVisualStyleBackColor = true;
+			// 
+			// txt1
+			// 
+			this.txt1.Location = new System.Drawing.Point(300, 160);
+			this.txt1.Name = "txt1";
+			this.txt1.Size = new System.Drawing.Size(100, 22);
+			this.txt1.TabIndex = 12;
+			// 
+			// txt2
+			// 
+			this.txt2.Location = new System.Drawing.Point(406, 160);
+			this.txt2.Name = "txt2";
+			this.txt2.Size = new System.Drawing.Size(100, 22);
+			this.txt2.TabIndex = 13;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(748, 756);
+			this.Controls.Add(this.txt2);
+			this.Controls.Add(this.txt1);
+			this.Controls.Add(this.lbl5);
+			this.Controls.Add(this.notBtn5);
+			this.Controls.Add(this.lbl4);
+			this.Controls.Add(this.notBtn4);
+			this.Controls.Add(this.lbl3);
+			this.Controls.Add(this.notBtn3);
+			this.Controls.Add(this.lbl2);
+			this.Controls.Add(this.notBtn2);
+			this.Controls.Add(this.lbl1);
+			this.Controls.Add(this.notBtn1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button notBtn1;
+		private System.Windows.Forms.Label lbl1;
+		private System.Windows.Forms.Button notBtn2;
+		private System.Windows.Forms.Label lbl2;
+		private System.Windows.Forms.Label lbl3;
+		private System.Windows.Forms.Button notBtn3;
+		private System.Windows.Forms.Label lbl4;
+		private System.Windows.Forms.Button notBtn4;
+		private System.Windows.Forms.Label lbl5;
+		private System.Windows.Forms.Button notBtn5;
+		private System.Windows.Forms.TextBox txt1;
+		private System.Windows.Forms.TextBox txt2;
+	}
+}
+
---
+++ Lesson_7/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_7
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void notBtn1_Click(object sender, EventArgs e)
+		{
+			for (Int32 i = 100; i<=120; i++)
+			{
+				lbl1.Text += i.ToString() + " ";
+				
+			}
+		}
+
+		private void notBtn2_Click(object sender, EventArgs e)
+		{
+			//works i hope
+			Int32 i = 100;
+			while (i <= 120)
+			{
+				lbl2.Text += i.ToString() + " ";
+				i++;
+			}
+		}
+
+		private void notBtn3_Click(object sender, EventArgs e)
+		{
+			AddNumbers(txt1.Text, txt2.Text);
+		
+		}
+
+		private String AddNumbers(string txt1, string txt2)
+		{
+			Decimal a = Convert.ToDecimal(txt1);
+			Decimal b = Convert.ToDecimal(txt2);
+			Int32 c = Convert.ToInt32(a);
+			Int32 d = Convert.ToInt32(b);
+			Int32 sum = c + d;
+			String final = Convert.ToString(sum);
+			lbl3.Text = final;
+			return final;
+		}
+	}
+}
